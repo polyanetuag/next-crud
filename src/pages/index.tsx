4a630f1eeabd1abdb55f4ad0fdc626b7352f2aa5
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-19 14:45:32 -0300

    testing tailwind properties

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div
+      className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-600 to-blue-600
+    `}
+    >
+      <span className="text-4xl">TEXTO</span>
+    </div>
+  );
+}
