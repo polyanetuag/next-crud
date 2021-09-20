d976ba389670f3975b4a8990ad7c997dda4ac975
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-20 11:09:31 -0300

    referencing Title and Layout components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
     <div
       className={`
       flex h-screen justify-center items-center
       bg-gradient-to-r from-purple-600 to-blue-600
+      text-white
     `}
     >
-      <span className="text-4xl">TEXTO</span>
+      <Layout title="Cadastro de Usuário Simples">
+        <span>Conteúdo</span>
+      </Layout>
     </div>
   );
 }
