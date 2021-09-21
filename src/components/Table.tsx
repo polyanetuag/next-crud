cbac56eb06d30ebf4bf6322e7abe84ed1d86dc9f
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-21 16:32:49 -0300

    styling the table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -9,9 +9,9 @@ export default function Table(props) {
     //dividindo em funções, mantém a renderização mais organizada
     return (
       <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
       </tr>
     );
   }
@@ -19,18 +19,28 @@ export default function Table(props) {
   function renderData() {
     return props.clients?.map((client, i) => {
       return (
-        <tr key={client.id}>
-          <td>{client.id}</td>
-          <td>{client.name}</td>
-          <td>{client.age}</td>
+        <tr
+          key={client.id}
+          className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
         </tr>
       );
     });
   }
 
   return (
-    <table>
-      <thead>{renderHeader()}</thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`
+        text-gray-100
+        bg-gradient-to-r from-purple-500 to-purple-800
+      `}
+      >
+        {renderHeader()}
+      </thead>
       <tbody>{renderData()}</tbody>
     </table>
   );
