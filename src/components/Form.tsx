f9de7c6117c22d675d7c27af2e24dc5ae8f71110
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-22 18:01:04 -0300

    form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Client from "../core/Client";
+import Button from "./Button";
+import Input from "./Input";
+
+interface FormProps {
+  client: Client;
+}
+
+export default function Form(props: FormProps) {
+  const id = props.client?.id;
+  const [name, setName] = useState(props.client?.name ?? "");
+  const [age, setAge] = useState(props.client?.age ?? 0);
+
+  return (
+    <div>
+      {id ? (
+        <Input onlyReading text="Código" value={id} className="mb-5" />
+      ) : (
+        false
+      )}
+      <Input text="Nome" value={name} onChange={setName} className="mb-5" />
+      <Input text="Idade" type="number" onChange={setAge} value={age} />
+      <div className="flex justify-end mt-7">
+        <Button color="blue" className="mr-2">
+          {id ? "Alterar" : "Salvar"}
+        </Button>
+        <Button>Cancelar</Button>
+      </div>
+    </div>
+  );
+}
