70dc7ed70f48b8cff1715174f7051ca4e9369190
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-22 18:02:01 -0300

    importing form component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import Form from "../components/Form";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import Client from "../core/Client";
@@ -33,11 +34,12 @@ export default function Home() {
             Novo cliente
           </Button>
         </div>
-        <Table
+        {/* <Table
           clients={clients}
           selectedCustomer={seletedClient}
           excludedCustomer={excludedClient}
-        />
+        /> */}
+        <Form client={clients[1]}></Form>
       </Layout>
     </div>
   );
