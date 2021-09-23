128987fbd3d0e8160192fc78fd0426201caa17c0
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-23 17:12:17 -0300

    adding onClick event and clientChaged function

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,8 @@ import Input from "./Input";
 
 interface FormProps {
   client: Client;
+  clientChanged: (client: Client) => void;
+  canceled?: () => void;
 }
 
 export default function Form(props: FormProps) {
@@ -22,10 +24,14 @@ export default function Form(props: FormProps) {
       <Input text="Nome" value={name} onChange={setName} className="mb-5" />
       <Input text="Idade" type="number" onChange={setAge} value={age} />
       <div className="flex justify-end mt-7">
-        <Button color="blue" className="mr-2">
+        <Button
+          color="blue"
+          className="mr-2"
+          onClick={() => props.clientChanged?.(new Client(name, +age, id))}
+        >
           {id ? "Alterar" : "Salvar"}
         </Button>
-        <Button>Cancelar</Button>
+        <Button onClick={props.canceled}>Cancelar</Button>
       </div>
     </div>
   );
